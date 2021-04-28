446c920ae6fae52d6de0a7ed10b2fb19f66a34ba
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-04-28 15:24:01 +1000

    Started creating prompts, added inquirer prompt method.

--- index.js
+++ index.js
@@ -1,13 +1,75 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'Project Name?',
+        name: 'projectName',
+    },
+    {
+        type: 'input',
+        message: 'Description?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Installation Command?',
+        name: 'installCommand',
+    },
+    {
+        type: 'input',
+        message: 'Usage?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Tests?',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'How to contribute?',
+        name: 'contribution',
+    },
+    {
+        type: 'list',
+        message: 'License Type?',
+        choices: [
+            "GPL",
+            "The Unlicense",
+            "Apache"
+        ],
+        name: 'License',
+    },
+    {
+        type: 'input',
+        message: 'Github Username?',
+        name: 'githubName',
+    },
+    {
+        type: 'input',
+        message: 'Email Address?',
+        name: 'email',
+    },
+  ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            console.log(answers);
+        })
+        .catch(error => {
+            console.log(error);   
+        });
+}
 
 // Function call to initialize app
 init();
