0b1d56a9580c81cbdd3703f3bfe85272a2f27a26
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-05-03 16:15:12 +1000

    Updated Inquirer messages to be more user friendly. Update list of license option to be more comprehensive

--- index.js
+++ index.js
@@ -1,57 +1,85 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
-        message: 'Project Name?',
+        message: "What is the name of your project?",
         name: 'projectName',
     },
     {
         type: 'input',
-        message: 'Description?',
+        message: 'How would you describe your project?',
         name: 'description',
     },
     {
         type: 'input',
-        message: 'Installation Command?',
+        message: 'How can your project be installed?',
         name: 'installCommand',
     },
     {
         type: 'input',
-        message: 'Usage?',
+        message: 'How can your project be used?',
         name: 'usage',
     },
     {
         type: 'input',
-        message: 'Tests?',
-        name: 'tests',
+        message: 'How can your project be tested?',
+        name: 'testCommand',
     },
     {
         type: 'input',
-        message: 'How to contribute?',
+        message: 'How to contribute to your project?',
         name: 'contribution',
     },
     {
         type: 'list',
-        message: 'License Type?',
+        message: 'What license would you like to use?',
         choices: [
-            "GPL",
+            "Apache 2.0 License",
+            "Boost Software License 1.0",
+            "BSD 3-Clause License",
+            "BSD 2-Clause License",
+            "CC0",
+            "Attribution 4.0 International",
+            "Attribution-ShareAlike 4.0 International",
+            "Attribution-NonCommercial 4.0 International",
+            "Attribution-NoDerivates 4.0 International",
+            "Attribution-NonCommmercial-ShareAlike 4.0 International",
+            "Attribution-NonCommercial-NoDerivatives 4.0 International",
+            "Eclipse Public License 1.0",
+            "GNU GPL v3",
+            "GNU GPL v2",
+            "GNU AGPL v3",
+            "GNU LGPL v3",
+            "GNU FDL v1.3",
+            "IBM Public License Version 1.0",
+            "ISC License (ISC)",
+            "The MIT License",
+            "Mozilla Public License 2.0",
+            "Open Data Commons Attribution",
+            "Open Database License (ODbL)",
+            "Public Domain Dedication and License (PDDL)",
+            "The Perl License",
+            "The Artistic License 2.0",
+            "SIL Open Font License 1.1",
             "The Unlicense",
-            "Apache"
+            "WTFPL",
+            "Zlib"
         ],
         name: 'License',
     },
     {
         type: 'input',
-        message: 'Github Username?',
+        message: 'What is your github username?',
         name: 'githubName',
     },
     {
         type: 'input',
-        message: 'Email Address?',
+        message: 'What is your email address?',
         name: 'email',
     },
   ];
