af54c69f5e4c75940d27e0c3e68313ca5e005978
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-05-03 23:02:47 +1000

    removed comments and unnecessary code

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
-
-// TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
@@ -96,8 +93,7 @@ function writeToFile(fileName, data) {
     var content = generateMarkdown(data);
     
     try {
-        const result = fs.writeFileSync(fileName+ '.md', content)
-        //file written successfully
+        fs.writeFileSync(fileName+ '.md', content)
       } catch (err) {
         console.error(err)
       }
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -199,11 +199,6 @@ function renderLicenseLink(license) {
   return licenseLink;
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
 
   var { projectName, 
@@ -215,7 +210,6 @@ function generateMarkdown(data) {
     license, 
     githubName, 
     email} = data
-  
   
   return `
 # ${projectName}
